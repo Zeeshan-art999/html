59b31e663dbfd4c7847d69b930c097dfec6000ed
Zeeshan <baog83600@gmail.com>
2025-07-31 08:43:48 -0700

    first commit

---
+++ Java Script/Class 01/script.js
@@ -0,0 +1,80 @@
+//var userNumberValue=123.4;      //numerical ma ager point aa  tu float data or agr na aai tu integar data//
+//console.log(userNumberValue);
+
+//var username1="Naveed's phone";
+//var username2='Ali"s phone';     //isko double,single,baketick kahta or issa jo kar skt jo kiya yahe hota//
+//var username3=`123${username1}`;
+//console.log(username3);//
+
+//var isFlag=true; //boolean data type hota or isme sirf true or false hi aata hai//
+//var nuliValue=null; //null data type hota or isme sirf null hi aata hai jiska mtlb isma koi value nai hoti//
+//var undefinedValue; //undefined data type hota or isme sirf undefined hi aata hai jiska mtlb isma koi value nai hoti//
+//var emptyValue=""; //empty string hota or isme koi value nai hoti//
+//var notANumber=NaN; //not a number hota or isme koi value nai hoti jiska mtlb isma koi value nai hoti//
+//var infinityValue=Infinity; //infinity hota or isme koi value nai hoti jiska mtlb isma koi value nai hoti//
+//var negativeInfinityValue=-Infinity; //negative infinity hota or isme koi value nai hoti jiska mtlb isma koi value nai hoti//
+
+
+//var userNumberValue=123;//camel case
+//var user_number_value=123;//snake case
+ //var UserNumberValue=123;//pascal case
+ //var USER_NUMBER_VALUE=123;//scereaming snake case
+//var user_number_Value=123;//kebabcase
+
+
+//Operators:-
+//1. Arithmetic Operators
+//+,-,*,/,%,++,--//
+
+var a=18;
+var b=20;
+
+
+var c = 2 + ++a - a--;
+console.log(c); 
+  
+
+//var c=++,
+//prefix increment
+//++a;
+//prefix decrement
+//--a;
+
+//var d=a++
+//postfix increment
+//a++;
+//postfix decrement
+//a--;
+
+
+
+//var c= 2%( 5+3)*2-1/2; modulus ko thora bara kar samjhaya
+//var c=26%9; //Modulus operator gives the remainder of the division  or agr divider bara ho tu choti wali valve lekhta hain
+//console.log(c);
+
+
+//DMAS=>Divison, Multiplication, Addition, Subtraction
+//var c = a+4+10-5*2/2+b;
+//console.log(c); 
+
+
+//var c=a+b;
+//console.log(c); //30
+//c=a-b;
+//console.log(c); //-10
+//c=a*b;
+//console.log(c); //200
+//c=a/b;
+//console.log(c); //0.5
+
+
+
+
+
+
+//2. Assignment Operators
+//3. Comparison Operators
+//4. Logical Operators
+//5. Bitwise Operators
+//6Unary Operators
+//7. Ternary Operators
