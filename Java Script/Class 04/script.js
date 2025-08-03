5f3e3562db6dbd314d853e98bbe5176a54310fa8
Zeeshan <baog83600@gmail.com>
2025-08-03 02:24:59 -0700

    loops and function

---
+++ Java Script/Class 04/script.js
@@ -0,0 +1,68 @@
+//function decaleration/definition
+function sum(num1,num2){             //()ka andar hota ha isko parameter kahta hain
+    var a =num1;
+    var b =num2;
+    var c =num1 + num2;
+    console.log(c);
+}
+
+sum(5,3);      //=>function calling
+
+sum("Zeeshan","Sarfraz");
+
+sum(7,3);
+
+
+
+//Built in function
+//user define function
+//function abc() {
+//  console.log("Hello World Function");
+//  console.log("Line 1");
+//}
+//console.log("Line 2");
+//console.log("Line 3");
+//console.log("Line 4");
+//console.log("Line 5");
+//abc();
+
+
+//for in loop     => ya be loops ha lakin inko abi nai parha  
+//for of loop
+
+
+//For loop:-
+//for(var i = 0; i < 10; i++){
+//    console.log("for",i);
+//}
+
+//for(var i=10; i>0;i--){
+//    console.log("for",i);
+//}
+
+//write a program that sum of the even number the arrange of 1 to 100 and if number is greater than 50
+//var sum=0;
+//for(var i=1;i<=100;i++){
+//    if(i >50 && i % 2 ==0) {
+//      sum = sum + i;
+//    }
+//}
+//console.log("sum =",sum);
+
+
+//Do while loop:-   agr condition false be ho tu ya  ak bar lazmi jala ga
+
+//var i=30;
+//do{
+//    console.log(i);
+//    i=i+10;
+//}while(i<10);
+
+
+//while loop:-
+
+//  var i=0;
+//while(i<10){
+//    console.log(i);
+//    i=i+5;
+//}
