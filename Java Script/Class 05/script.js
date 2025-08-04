27a1513273d06cd60f1761ac2872d8a101aa5af6
Zeeshan <baog83600@gmail.com>
2025-08-03 18:57:05 -0700

    function or built in function

---
+++ Java Script/Class 05/script.js
@@ -0,0 +1,49 @@
+//Built in functions in java script:-
+
+//num1 = num1.toString();
+//var result = Math.random()*50;
+//result = Math.pow(2,3);
+//result = Math.max(1,2,3,4,5);
+//result = Math.min(1,2,3,4,5);        =>ya sb bana banai function hota hain huma logic lekhna ki zarrorat nai parti hum jo jaha function
+                                     //use kar skta. bht sa function ha yahan pr kuch he lekha hain
+//result = Math.round(7.6);
+//result = Math.floor(7.8);
+//result = Math.ceil(7.3);
+//result=parseInt(result);
+//var output = "2.3";
+//output = parseFloat(output);
+
+
+
+
+//function add(...agrs){              //spread opertor => isma hum jitna jahein parameter baj skta 
+//    return agrs ;
+//}
+//var result = add(2,3,4);
+//console.log("result",result);
+
+
+//function add(a=4,b=3){               //=>default parameter
+//    var c = a + b ;
+//    return c ; 
+//}
+//var result = add(10);
+// console.log("result",result);
+
+
+//function multiply(c,d){
+//    return c * d ;
+//}
+
+//function add(a,b){
+//       c= a + b ;
+                                        //=>isma phly function call hota or jisko call karta wo run hota or return sa vavle dobara call ma jati
+                                        //function ma multiple parameter hota lakin return ak he use ho skta 
+//       var y = multiply(c,b); 
+//       return y;
+// }
+// var result = add(2,5);
+//console.log("result",result);
+
+//var output = add(result,10);
+ 
